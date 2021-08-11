d16b48af0ceee29025dfad170e6d850d25b5cf2b
Fernando <fdo.clarosmith@gmail.com>
2021-08-11 16:48:54 -0400

    Minor fixes

--- params.py
+++ params.py
@@ -4,5 +4,5 @@ import os
 VERSION = "PoC"
 PATH_LOGIN_WINDOW = os.path.join("frontend", "ventanas", "ventanaLogin.ui")
 PATH_RAMOS_WINDOW = os.path.join("frontend", "ventanas", "ventanaPrincipal.ui")
-SIZE_RAMOS_WINDOW = (937, 659)
+SIZE_RAMOS_WINDOW = (1032, 659)
 SIZE_LOGIN_WINDOW = (434, 376)
