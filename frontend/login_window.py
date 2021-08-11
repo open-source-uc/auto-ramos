88329b4ca1bbb4c6d9fc7ce476c8e301e8e5227e
Fernando <fdo.clarosmith@gmail.com>
2021-08-11 12:48:44 -0400

    Creada gui funcional

--- frontend/login_window.py
+++ frontend/login_window.py
@@ -1,47 +1,38 @@
+from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QHBoxLayout, QLabel, QPushButton, QVBoxLayout, QWidget, QLineEdit, QMessageBox
+import params
+from PyQt5 import uic
+
+window_name, base_class = uic.loadUiType(params.PATH_LOGIN_WINDOW)
 
 
-class LoginWindow(QWidget):
+class LoginWindow(window_name, base_class):
+
+    switch_window_signal = pyqtSignal()
+
     def __init__(self) -> None:
         super().__init__()
+        self.setupUi(self)
         self.init_ui()
 
     def init_ui(self) -> None:
-        self.main_vbox = QVBoxLayout()
-        self.setLayout(self.main_vbox)
-
-        # LINEAS DE INGRESO DE NOMBRE Y CLAVE
-        self.username_layout = QHBoxLayout()
-        self.username_label = QLabel('Usuario:')
-        self.username_box = QLineEdit()
-        
-        self.username_layout.addWidget(self.username_label)
-        self.username_layout.addWidget(self.username_box)
-
-        self.password_layout = QHBoxLayout()
-        self.password_label = QLabel('Contraseña:')
-        self.password_box = QLineEdit()
-
-        # Asi la clave no se ve cuando la escribes
-        self.password_box.setEchoMode(QLineEdit.Password)
-
-
-        self.password_layout.addWidget(self.password_label)
-        self.password_layout.addWidget(self.password_box)
-        
-        self.main_vbox.addLayout(self.username_layout)
-        self.main_vbox.addLayout(self.password_layout)
-
-        #Boton login
-        self.login_button = QPushButton('Login')
-        self.main_vbox.addWidget(self.login_button)
-
-
         self.invalid_session_popup = QMessageBox(self)
         self.invalid_session_popup.setWindowTitle('ERROR')
         self.invalid_session_popup.setText(
             "USUARIO O CONTRASEÑA INCORRECTAS")
         self.invalid_session_popup.setIcon(QMessageBox.Critical)
 
+        self.login_button.clicked.connect(self.switch_window)
+        self.mostrar_clave_checkbox.stateChanged.connect(self.show_password)
+
+    def show_password(self):
+        if self.mostrar_clave_checkbox.isChecked():
+            self.password_box.setEchoMode(QLineEdit.Normal)
+        else:
+            self.password_box.setEchoMode(QLineEdit.Password)
+
     def raise_popup(self):
         self.invalid_session_popup.exec_()
+
+    def switch_window(self):
+        self.switch_window_signal.emit()
